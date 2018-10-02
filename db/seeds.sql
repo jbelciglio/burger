aa7b0b67bee077efede13f8211bd3c1832995d52
jbelciglio <jbelciglio@gmail.com>
2018-10-01 20:41:40 -0700

    In Progress

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name) VALUES ("50 Ways to Leave Your Guava Burger");
+INSERT INTO burgers (name) VALUES ("Peas and Thank You Burger");
+INSERT INTO burgers (name) VALUES ("Sweet Home Avocado Burger");
